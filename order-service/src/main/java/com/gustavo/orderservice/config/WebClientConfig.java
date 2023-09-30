289aeb5c45d634bee90ecdb11ba5c09a99657854
Gustavo Cruz <gu.cruz17@hotmail.com>
2023-09-30 18:49:26 -0300

    Service Discovery

--- order-service/src/main/java/com/gustavo/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/gustavo/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.gustavo.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,8 +9,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 	
 	@Bean
-	public WebClient webClient() {
-		return WebClient.builder().build();
+	@LoadBalanced // Adiciona os recursos de balanceamento de carga do lado do cliente
+	public WebClient.Builder webClientBuilder() {
+		return WebClient.builder();
 	}
 	
 }
